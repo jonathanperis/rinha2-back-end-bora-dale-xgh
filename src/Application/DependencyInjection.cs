157f7b0c29f1b35278f876d39b6b905a87cb27eb
Jonathan Peris <jperis.silva@gmail.com>
2024-02-13 12:29:22 -0300

    Update DependencyInjection.cs

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
-
-namespace Application;
+﻿namespace Application;
 
 public static class DependencyInjection
 {
     public static void AddApplication(this IServiceCollection services)
     {
-        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        //services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         services.AddMediator(options =>
         {
             options.ServiceLifetime = ServiceLifetime.Scoped;
         });
+
+        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidatorBehavior<,>));
     }
 }
