6fbc0f978f1b66582feb4d61137d88bffde6fcef
Jonathan Peris <jperis.silva@gmail.com>
2024-02-15 19:06:39 -0300

    Chore: Migração do banco de dados

---
+++ src/Infrastructure/Migrations/20240215214900_InitialMigration.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Limite = table.Column<int>(type: "integer", nullable: false),
+                    SaldoInicial = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transacoes",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Valor = table.Column<int>(type: "integer", nullable: false),
+                    ClienteId = table.Column<int>(type: "integer", nullable: false),
+                    Tipo = table.Column<char>(type: "character(1)", nullable: false),
+                    Descricao = table.Column<string>(type: "text", nullable: false),
+                    RealizadoEm = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transacoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transacoes_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalSchema: "public",
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "public",
+                table: "Clientes",
+                columns: new[] { "Id", "Limite", "SaldoInicial" },
+                values: new object[,]
+                {
+                    { 1, 100000, 0 },
+                    { 2, 80000, 0 },
+                    { 3, 1000000, 0 },
+                    { 4, 10000000, 0 },
+                    { 5, 500000, 0 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacoes_ClienteId",
+                schema: "public",
+                table: "Transacoes",
+                column: "ClienteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transacoes",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "Clientes",
+                schema: "public");
+        }
+    }
+}
