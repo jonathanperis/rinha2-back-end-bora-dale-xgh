912d1a37aa2526be9e7cef078db9f57bccfc2cc5
Jonathan Peris <jperis.silva@gmail.com>
2024-02-26 08:47:39 -0300

    Fix:

--- src/Application/Common/Dtos/TransacaoDto.cs
+++ src/Application/Common/Dtos/TransacaoDto.cs
@@ -2,15 +2,12 @@
 
 public sealed record TransacaoDto
 {
-    [Required]
     [Range(1, int.MaxValue)]
     public int Valor { get; set; }
     
-    [Required]
     [AllowedValues('c', 'd')]
     public char Tipo { get; set; }
 
-    [Required]
     [MaxLength(10)]
     public string Descricao { get; set; } = string.Empty;
 }
