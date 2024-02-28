0879afa8b0f1e8adcf8b50531f45bc4ec4ed261f
Jonathan Peris <jperis.silva@gmail.com>
2024-02-28 16:26:03 -0300

    Update TransacaoDto.cs

--- src/Application/Common/Dtos/TransacaoDto.cs
+++ src/Application/Common/Dtos/TransacaoDto.cs
@@ -2,5 +2,5 @@
 
 public readonly record struct TransacaoDto(int Valor, string? Tipo)
 {
-    public string Descricao { get; } = string.Empty;
+    public string Descricao { get; init; } = string.Empty;
 }
