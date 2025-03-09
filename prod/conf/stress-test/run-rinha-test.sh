40335bcadfcba4d5ab37789bdd9c80c88a64427b
Jonathan Peris <jperis.silva@gmail.com>
2025-03-08 23:12:28 -0300

    Update run-rinha-test.sh

--- prod/conf/stress-test/run-rinha-test.sh
+++ prod/conf/stress-test/run-rinha-test.sh
@@ -3,4 +3,8 @@
 echo "Tests will start in 15 seconds..."
 sleep 15
 
+chmod -c -R +rX "_site/" | while read line; do
+    echo "::warning title=Invalid file permissions automatically fixed::$line"
+done
+
 k6 run rinha-test.js
