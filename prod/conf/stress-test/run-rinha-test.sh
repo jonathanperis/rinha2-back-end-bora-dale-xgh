1101f8be299f8428fcae6c8337c76e03022e232f
Jonathan Peris <jperis.silva@gmail.com>
2025-03-09 09:16:04 -0300

    Atualizar o run-rinha-test.sh

--- prod/conf/stress-test/run-rinha-test.sh
+++ prod/conf/stress-test/run-rinha-test.sh
@@ -3,10 +3,12 @@
 echo "Tests will start in 15 seconds..."
 sleep 15
 
+# Run the k6 test
 k6 run rinha-test.js
 
-# After the test run, copy the exported report to the mounted /reports directory
+# Fix the permissions on the generated report before copying it.
 if [ -f stress-test-report.html ]; then
+    chmod 644 stress-test-report.html
     cp stress-test-report.html /reports/
     echo "Report copied to /reports/stress-test-report.html"
 else
