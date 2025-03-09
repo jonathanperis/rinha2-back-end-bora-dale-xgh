3c006da3ec8ac4336e0ba584d522ae1411b355fc
Jonathan Peris <jperis.silva@gmail.com>
2025-03-09 00:13:07 -0300

    Atualizar o run-rinha-test.sh

--- prod/conf/stress-test/run-rinha-test.sh
+++ prod/conf/stress-test/run-rinha-test.sh
@@ -3,8 +3,12 @@
 echo "Tests will start in 15 seconds..."
 sleep 15
 
-chmod -c -R +rX "_site/" | while read line; do
-    echo "::warning title=Invalid file permissions automatically fixed::$line"
-done
-
 sudo k6 run rinha-test.js
+
+# After the test run, copy the exported report to the mounted /reports directory
+if [ -f stress-test-report.html ]; then
+    cp stress-test-report.html /reports/
+    echo "Report copied to /reports/stress-test-report.html"
+else
+    echo "Report not found!"
+fi
